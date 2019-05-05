538c0830fdbd0f889d06e0a1de4ba0a767f07427
Ivan <ivantm24@gmail.com>
2019-05-05 10:05:09 -0400

    mejora visual de las pruebas

--- c_testing_from_exe/plantilla_test_c.py
+++ c_testing_from_exe/plantilla_test_c.py
@@ -13,13 +13,31 @@ class PlantillaPrueba(PruebaAbstracta):
     # En yo.veo, solo pon lo que el programa escribe en stdout; NO incluir lo digitado por el usuario en stdin
     # todas las pruebas tienen que comenzar con test
     def test_suma_positiva(yo):
-        yo.entro(b"1\r\n3\r\n")
-        yo.veo(b'\x82Digita 2 #s: La suma es: 4\r\n')
+        yo.entro(
+            b"1\r\n"
+            b"3\r\n"
+        )
+        yo.veo(
+            b"\x82Digita 2 #s: "
+            b"La suma es: 4\r\n"
+        )
 
     def test_negativa(yo):
-        yo.entro(b"-1\n-4\n")
-        yo.veo(b'\x82Digita 2 #s: La suma es: -5\r\n')
+        yo.entro(
+            b"-1\n"
+            b"-4\n"
+        )
+        yo.veo(
+            b"\x82Digita 2 #s: "
+            b"La suma es: -5\r\n"
+        )
 
     def test_pos_neg(yo):
-        yo.entro(b"-1\n4\n")
-        yo.veo(b'\x82Digita 2 #s: La suma es: 3\r\n')
+        yo.entro(
+            b"-1\n"
+            b"4\n"
+        )
+        yo.veo(
+            b"\x82Digita 2 #s: "
+            b"La suma es: 3\r\n"
+        )
