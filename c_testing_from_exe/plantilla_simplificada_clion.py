95c08dd0860c15237709bddf5ca68efb86ee1b11
Ivan <ivantm24@gmail.com>
2019-06-13 11:57:17 -0400

    quita comentario y remueve ConfigVar

--- c_testing_from_exe/plantilla_simplificada_clion.py
+++ c_testing_from_exe/plantilla_simplificada_clion.py
@@ -1,15 +1,7 @@
 from c_testing_from_exe.abstract_test_case import PruebaAbstracta
 
-# Recuerda Compilar con las opciones -static-libgcc y -static-libstdc++
-# En CodeBlocks, click en Project -> Build options
-# Si compilas sin las opciones en CodeBlocks, tendrás que modificar el código fuente para que coja las opciones
 
 class PlantillaPrueba(PruebaAbstracta):
-
-    def configVar(self):
-        # CodeBlocks crea el ejecutable en {RutaFolderProjecto}\bin\Debug\{NombreProyecto}.exe
-        self.ejecutable = r"..\suma2\bin\Debug\suma2.exe"
-
     # En yo.veo, solo pon lo que el programa escribe en stdout; NO incluir lo digitado por el usuario en stdin
     # todas las pruebas tienen que comenzar con test
     def test_suma_positiva(yo):
