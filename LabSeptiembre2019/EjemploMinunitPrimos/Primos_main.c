46970cb80e10b05ae977a9c7019b0edc21177285
Ivan <ivantm24@gmail.com>
2019-10-03 16:28:42 -0400

    agrega ejemplo clioin

---
+++ LabSeptiembre2019/EjemploMinunitPrimos/Primos_main.c
@@ -0,0 +1,11 @@
+//
+// Created by Ivan's PC on 10/3/2019.
+//
+
+#include <stdio.h>
+#include "miLibreria.h"
+
+int main() {
+
+    return 0;
+}
---
+++ LabSeptiembre2019/EjemploMinunitPrimos/Primos_minunit.c
@@ -0,0 +1,48 @@
+//
+// Created by Ivan Tactuk on 10/3/2019.
+//
+
+#include <minunit.h>
+
+#include "miLibreria.h"
+
+#define MINUNIT_EPSILON 1E-5
+
+MU_TEST(test_esprimo2) {
+    mu_assert_int_eq(1, esprimo(2));
+}
+
+MU_TEST(test_esprimo23) {
+    mu_assert_int_eq(1, esprimo(23));
+}
+
+MU_TEST(test_esprimo16) {
+    mu_assert_int_eq(0, esprimo(16));
+}
+
+MU_TEST(test_esprimoError) {
+    mu_assert_int_eq(-1, esprimo(-4));
+}
+
+MU_TEST(test_PromedioVacio) {
+    mu_assert_double_eq(0.0, calc_promedio_numeros_primos(20,22));
+}
+
+MU_TEST(test_PromedioPequeno) {
+    mu_assert_double_eq(6.83333, calc_promedio_numeros_primos(2,13));
+}
+
+MU_TEST_SUITE(test_suite) {
+    MU_RUN_TEST(test_esprimo2);
+    MU_RUN_TEST(test_esprimo23);
+    MU_RUN_TEST(test_esprimo16);
+    MU_RUN_TEST(test_esprimoError);
+    MU_RUN_TEST(test_PromedioVacio);
+    MU_RUN_TEST(test_PromedioPequeno);
+}
+
+int main(int argc, char *argv[]) {
+    MU_RUN_SUITE(test_suite);
+    MU_REPORT();
+    return 0;
+}
