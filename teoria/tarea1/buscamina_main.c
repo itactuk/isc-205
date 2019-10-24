48b03bfa60d08e619b737514b37ee7dc88245fa9
itactuk <46655567+itactuk@users.noreply.github.com>
2019-10-24 16:57:03 -0400

    Update buscamina_main.c

--- teoria/tarea1/buscamina_main.c
+++ teoria/tarea1/buscamina_main.c
@@ -11,7 +11,7 @@ int inicializa(int matriz[90][90], int n, int m);
 int gano(int matriz[90][90], int n, int m, int bombas);
 int termino(int matriz[90][90], int n, int m, int bombas);
 int click(int matriz_visual[90][90], int matriz_valores[90][90], int n, int m, int i, int j);
-void explosion(int matriz[90][90], int n, int m, int i, int j);
+void explosion(int matriz_valores[90][90], int matriz_visual[90][90], int matriz_visitados[90][90], int n, int m, int i, int j);
 int generar_bombas(int matriz[90][90], int n, int m, int bomba);
 int generar_numeros(int matriz[90][90], int n, int m);
 void sumar_uno(int matriz[90][90], int n, int m, int i, int j);
